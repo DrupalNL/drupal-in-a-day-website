d5fec532f60297795a6ee176753a7068c4c24061
Paul Rijke <paul@rijke.org>
2017-05-31 16:28:31 +0200

    Browersync toevoegen nog

---
+++ web/themes/custom/diad/gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var livereload = require('gulp-livereload')
+var uglify = require('gulp-uglifyjs');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+// nog te doen
+// var browsersync = require('browser-sync');
+
+
+
+
+gulp.task('imagemin', function () {
+    return gulp.src('./images/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./images'));
+});
+
+
+gulp.task('sass', function () {
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function(){
+    livereload.listen();
+
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    // gulp.watch('./wp-content/themes/olympos/lib/*.js', ['uglify']);
+    gulp.watch(['./css/style.css', './**/*.twig'], function (files){
+        livereload.changed(files)
+    });
+});
