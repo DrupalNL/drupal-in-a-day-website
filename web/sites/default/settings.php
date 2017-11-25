c3b2abdbb6ade928ce3f504d8ed900bf76f76a52
Sean B <s.p.blommaert@gmail.com>
2017-11-25 13:47:34 +0100

    Added platform specific site config.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -6,10 +6,15 @@
  */
 
 /**
+ * Database settings.
+ */
+$databases = [];
+
+/**
  * Location of the site configuration files.
  */
 $config_directories = [
-  'sync' => './../config/sync',
+  CONFIG_SYNC_DIRECTORY => './../config/sync',
 ];
 
 /**
@@ -110,7 +115,7 @@ $settings['file_private_path'] = 'sites/default/files/private';
 /**
  * Load services definition file.
  */
-$settings['container_yamls'][] = __DIR__ . '/services.yml';
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 
 /**
  * The default list of directories that will be ignored by Drupal's file API.
@@ -120,9 +125,14 @@ $settings['file_scan_ignore_directories'] = [
   'bower_components',
 ];
 
+// Automatic Platform.sh settings.
+if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
+  include $app_root . '/' . $site_path . '/settings.platformsh.php';
+}
+
 /**
  * Load local development override configuration, if available.
  */
-if (file_exists(__DIR__ . '/settings.local.php')) {
-  include __DIR__ . '/settings.local.php';
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
 }
